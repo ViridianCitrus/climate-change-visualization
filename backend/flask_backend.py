8f24e4d07300920a4c5cc22fd89d452861f6c164
MTahaK <taha.khokhar@mail.utoronto.ca>
2021-08-21 17:45:51 -0400

    Progress on nearest stations functionality

--- backend/flask_backend.py
+++ backend/flask_backend.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import math
 from flask import Flask, request
 
 
@@ -10,11 +11,19 @@ DATABASE = os.path.join(DATABASE_PATH, 'climatedata.db')
 
 app = Flask(__name__)
 
+
+def euclidean_distance(lat1, lat2, lon1, lon2):
+    lat_comp = (lat2 - lat1) ** 2
+    lon_comp = (lon2-lon1) ** 2
+
+    return math.sqrt(lat_comp + lon_comp)
+
+
 # TODO: Change routes as needed to match front end
 @app.route("/api/region")
 def station_return():
     """
-    Returns given station's climate data upon request
+    Returns given station's climate data
     """
     sid = request.args['sid']
     db = sqlite3.connect(DATABASE)
@@ -27,19 +36,28 @@ def station_return():
     return output
 
 @app.route("/api/region/local")
-def station_local_return():
+def local_return():
     """
-    Returns the climate data of the given station and the closest 4 stations
-    upon request
+    Returns the climate data of the five closest stations to the selected
+    location
     """
-    sid = request.args['sid']
-    # num = request.args['num']
+    num = 5
+    lat = request.args['lat']
+    lon = request.args['lon']
+    sel_pos = (lat, lon)
+
     db = sqlite3.connect(DATABASE)
     cur = db.cursor()
-    data = cur.execute('SELECT * FROM CLIMATE_DATA WHERE station_id = ?;', (sid,))
-    output = dict()
-    for line in data:
-        output[line[5]] = line[6]
+    data = cur.execute('SELECT * FROM CLIMATE_DATA')
+
+    station_ids = list()
+    count = 0
+    while count <= num:
+        id =
+        for line in data:
+            pos = (line[0], line[1])
+
+            output[line[5]] = line[6]
     cur.close()
     return output
 
