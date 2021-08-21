c06de1e301465c0fe51f5e45c04cc584afdae9ed
Zeyu-Li <zeyu.li23@gmail.com>
2021-08-21 10:51:55 -0600

    started home page

--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -3,13 +3,21 @@ import { Navbar } from "./Navbar";
 
 export const MainPage: React.FC = () => {
   useEffect(() => {
-    document.title = "Climate Report | Main";
+    document.title = "Climate Report | Home";
   }, []);
 
   return (
     <>
       <Navbar />
-      <p>Main Page</p>
+      <div className="home overlay">
+        <div>
+          <h1>Climate Report</h1>
+          <p>See the change to your world</p>
+          <button style={{ marginRight: "50px", marginTop: "50px" }}>
+            <p>Get Report</p>
+          </button>
+        </div>
+      </div>
     </>
   );
 };
