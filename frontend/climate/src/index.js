683b970cc9af3cb37a3baa631100eb2c98458096
Zeyu-Li <zeyu.li23@gmail.com>
2021-08-21 10:59:42 -0600

    Revert "Merge pull request #2 from ViridianCitrus/Kevin"
    This reverts commit e433bff11d2b56c956792f3518a34b9bb7fb80db, reversing
    changes made to 34e610afe4d2327b636128296fcb909617cff5de.

--- frontend/climate/src/index.js
+++ frontend/climate/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Intro from './Intro';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Intro />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
