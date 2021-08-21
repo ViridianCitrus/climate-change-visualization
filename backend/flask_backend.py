bd5b1c0a66ee07a55aaa5d2d2fd9e98790b26e1c
MTahaK <taha.khokhar@mail.utoronto.ca>
2021-08-21 17:12:09 -0400

    Started code for finding nearest stations to a given station
    Also copied climatedata.db into the 'data' directory - change if desired

--- backend/flask_backend.py
+++ backend/flask_backend.py
@@ -1,24 +1,47 @@
 import os
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+from flask import Flask, request
+
 
 BACKEND_PATH = os.path.dirname(os.path.realpath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(BACKEND_PATH, '..'))
 DATABASE_PATH = os.path.join(PROJECT_ROOT, 'data')
-DATABASE = os.path.join(DATABASE_PATH, 'climate_data.db')
+DATABASE = os.path.join(DATABASE_PATH, 'climatedata.db')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////" + DATABASE
-db = SQLAlchemy(app)
-
 
 # TODO: Change routes as needed to match front end
-@app.route("/api/region/<string:city>")
-def loc_return(city):
+@app.route("/api/region")
+def station_return():
     """
-    Return a .json of the given city's climate data upon request
+    Returns given station's climate data upon request
     """
-    return jsonify()
+    sid = request.args['sid']
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    data = cur.execute('SELECT * FROM CLIMATE_DATA WHERE station_id = ?;', (sid,))
+    output = dict()
+    for line in data:
+        output[line[5]] = line[6]
+    cur.close()
+    return output
+
+@app.route("/api/region/local")
+def station_local_return():
+    """
+    Returns the climate data of the given station and the closest 4 stations
+    upon request
+    """
+    sid = request.args['sid']
+    # num = request.args['num']
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    data = cur.execute('SELECT * FROM CLIMATE_DATA WHERE station_id = ?;', (sid,))
+    output = dict()
+    for line in data:
+        output[line[5]] = line[6]
+    cur.close()
+    return output
 
 
 if __name__ == '__main__':
