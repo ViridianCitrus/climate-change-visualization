5e81be48c2b5dfbc0882737e13dca63e39af6b9e
Ben Wang <xiaoyu.w.99@gmail.com>
2021-08-21 18:47:19 -0600

    idk this doesn't work :(

--- frontend/src/Report.tsx
+++ frontend/src/Report.tsx
@@ -1,15 +1,47 @@
 import React, { useState, useEffect } from "react";
 
+
 import { ReactComponent as TriangleOpen } from "./images/triangleOpen.svg";
 import { ReactComponent as TriangleClose } from "./images/triangleClose.svg";
 import { Navbar } from "./Navbar";
 
+import { geoToH3 } from "h3-js";
+import { StaticMap } from "react-map-gl";
+
+//@ts-ignore
+import DeckGL from '@deck.gl/react';
+//@ts-ignore
+import { H3HexagonLayer } from '@deck.gl/geo-layers';
+import tempdata from './testdata.json';
+
 export const Report: React.FC = () => {
+  const MAPBOX_TOKEN = "pk.eyJ1IjoiYmVuYWRyaWxsIiwiYSI6ImNrc21hajlrbzFqaGoydXBjOWlyOGl5cHIifQ.sTt3_tgmpDlBUOaPW7lTqg"
+
   // const [offsetHeight, setOffsetHeight] = useState(100);
   // const [sidebarOffset, setSidebarOffset] = useState(0);
   const [showType, changeShowType] = useState("None");
   const [searchField, changeSearchField] = useState("");
   const [toggleSidebar, toggleToggleSidebar] = useState(true);
+  // const [viewport, setViewport] = useState({
+  //   latitude: 43.6532,
+  //   longitude: -79.3832,
+  //   width: "75vw",
+  //   height: "100vh",
+  //   zoom: 10
+  // })
+  const layer = new H3HexagonLayer({
+    id: 'h3-hexagon-layer',
+    tempdata,
+    pickable: true,
+    wireframe: false,
+    filled: true,
+    extruded: true,
+    elevationScale: 20,
+    getHexagon: (d: any) => geoToH3(d.latitude, d.longitude, 0),
+    getFillColor: (d: any) => [255, (1 - d.temp / 10) * 255, 0],
+    getElevation: (d: any) => d.temp * 10
+  })
+
 
   useEffect(() => {
     document.title = "Climate Report | Report";
@@ -18,6 +50,15 @@ export const Report: React.FC = () => {
   useEffect(() => {
     // TODO: get the right data type to display
   }, [showType]);
+
+
+  const INITIAL_VIEW_STATE = {
+    latitude: 43.6532,
+    longitude: -79.3832,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+  };
 
   return (
     <>
@@ -34,6 +75,15 @@ export const Report: React.FC = () => {
             onChange={(e) => changeSearchField(e.target.value)}
           />
           {/* timeline */}
+        </div>
+        <div>
+          <DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            layers={[layer]}
+          >
+            <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} mapStyle={"mapbox://styles/benadrill/cksmdc7bynkzp17ly78uzonu7"} />
+          </DeckGL>
         </div>
         <div
           className={`sidebar transition ${
